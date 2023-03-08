12f0ef2c6d9f15eec103938f42821238bd1e1926
home <home@DESKTOP-3O675RG>
2023-03-08 17:24:04 +0300

    Добавьте файлы проекта.

---
+++ Alpnism1/AboutUs.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Alpnism1
+{
+    /// <summary>
+    /// Логика взаимодействия для AboutUs.xaml
+    /// </summary>
+    public partial class AboutUs : Window
+    {
+        public AboutUs()
+        {
+            InitializeComponent();
+        }
+        private void PackIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void MainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+        private void Move_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
